8e768993551e23a99e73f24c3544d1ead0ba523d
Michelle Rogers <michelle@merogers.dev>
2022-07-04 14:01:28 -0700

    initial commit for 1.2.1

--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,6 @@
-const mongoose = require("mongoose");
-const express = require("express");
-const app = express();
-
-const itemRoutes = require("./routes/itemRoutes");
-
-// --- ENV --- //
+// ENV
 
 require("dotenv").config();
+const PORT = process.env.PORT || 5000;
 
-const port = process.env.PORT || 5000;
-const server = process.env.SERVER;
-const database = process.env.DB;
-
-// --- Connect to MongoDB, then start Express --- //
-
-const startBackend = async () => {
-  try {
-    await mongoose.connect(`${server}/${database}`, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log(`> Connected to database`);
-    app.listen(port, () => {
-      console.log(`> GLA-REACT is listening on port: ${port}...`);
-    });
-  } catch (error) {
-    console.error("> Error connecting to DB", error);
-  }
-};
-
-startBackend();
-
-// --- Routes --- //
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use("/api", itemRoutes);
-
-app.get("/", (req, res) => {
-  res.status(200).send("GLA App Backend").end();
-});
-
-module.exports = app;
+// Express
