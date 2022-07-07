b1d49a108ecf3a6ee0f8f8a2438e9f168ea4d201
Michelle Rogers <michelle@merogers.dev>
2022-07-07 15:54:04 -0700

    added gcp appengine files

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,23 +1,4 @@
-// Connect to Database
-
-// const mongoose = require("mongoose");
-
-// const connectDB = async () => {
-//   try {
-//     const mongoDBConnect = await mongoose.connect(process.env.MONGO_URI, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     console.log(`> MongoDB Connected to: ${mongoDBConnect.connection.host}`);
-//   } catch (error) {
-//     console.log(error);
-//     process.exit(1);
-//   }
-// };
-
 require("dotenv").config();
-
-// module.exports = { connectDB };
 
 const mysql = require("mysql2/promise");
 
