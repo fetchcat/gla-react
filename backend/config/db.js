418f917d7f679e1432b30247b05f02c4e203a6d5
Michelle Rogers <michelle@merogers.dev>
2022-07-07 21:11:15 -0700

    added socket path to db config

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,14 @@ require("dotenv").config();
 
 const mysql = require("mysql2/promise");
 
+const dbSocketPath = process.env.DB_SOCKET_PATH || "/cloudsql";
+
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
+  // host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  socketPath: `${dbSocketPath}/${process.env.INSTANCE_CONNECTION_NAME}`,
 });
 
 module.exports = pool;
